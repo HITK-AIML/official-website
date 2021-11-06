7865acca99c73a489b3cff1433dc5082ac2ac3cc
Roshan <roshan0902@pm.me>
2021-11-06 22:53:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+app.use(express.static('public/img'));
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+app.listen(port, ()=>{
+    console.log('Server active');
+})
