7e887d4a65486446b27266758b29029929ed60f5
HITK-AIML <93835264+HITK-AIML@users.noreply.github.com>
2021-11-19 12:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
